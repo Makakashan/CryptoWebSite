ac75b261a61c85948ff5f8c9ce71e5f2e7aaf37f
Makakashan <vacetmax@gmail.com>
2026-05-08 00:54:33 +0200

    feat: add API index

---
+++ frontend-project/src/api/index.ts
@@ -0,0 +1,6 @@
+export { default as api } from "./axiosConfig";
+export { assetsApi } from "./assetsApi";
+export { authApi } from "./authApi";
+export { ordersApi } from "./ordersApi";
+export { portfolioApi } from "./portfolioApi";
+export { profileApi } from "./profileApi";
